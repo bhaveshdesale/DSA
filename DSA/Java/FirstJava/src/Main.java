c12548be4d71d9fdcb2e36670da04b9ff3446852
Bhavesh <143746617+bhaveshdesale@users.noreply.github.com>
2025-01-03 17:09:03 +0530

    This is lecture no 1

---
+++ DSA/Java/FirstJava/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // string[] is an array and its an command line argument like
+        // whatever you give you in cammand line as an array it will be stored in args variable
+        //static is used to run the class without creating an object
+
+        System.out.println("Hello world!");
+        System.out.println("Hi this is bhavesh");
+        Scanner sc=new Scanner(System.in);
+        int a;
+        //primitive in simple that you can not that datatype into further
+
+        a=sc.nextInt();
+        System.out.println(a);
+    }
+
+}
